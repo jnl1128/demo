c8f8e2e40623455da77a47695959bba44490e86a
Narin <jnl1128@naver.com>
2020-09-07 05:26:55 +0900

    first github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./element.css";
+import "./board.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className = "a"></div>
+        <div className = "b"></div>
+        <div className = "c"></div>
+        <div className = "d"></div>
+        <div className = "e"></div>
+        <div className = "f"></div>
+        <div className = "g"></div>
+        <div className = "h"></div>
+        <div className = "add_post"></div>
+        <div className = "consult"></div>
+
+        <div className = "notice_board"></div>
+        <div className = "zoom_board"></div>
+        <div className = "chat_board"></div>
+        <div className = "attending_board"></div>
+        <div className = "qanda_board"></div>
+        <div className = "memo_board"></div>
+      </div>
+
+    )
+  }
 }
 
 export default App;
