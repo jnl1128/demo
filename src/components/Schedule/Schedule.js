26d29d1275ed01f0be662d4b240180d864568555
Narin <jnl1128@naver.com>
2020-09-08 21:26:43 +0900

    calendar_size

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { Link } from 'react-router-dom';
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Header, Image, Message, Segment, Container, Divider } from 'semantic-ui-react'
 
 class Schedule extends Component {
 
@@ -19,14 +18,34 @@ class Schedule extends Component {
         html, body {
       background-color: white !important;
     }
-
+    p1{
+      align-content: center;
+      background-color: #AFC99F;
+      color: #FFFFFF;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      min-height : 20em;
+    }
     `}</style>
-
-    
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-        />
+    <Container text style={{marginTop: '7em' }}>
+      <p1>
+      <Calendar
+        onChange={this.onChange}
+        value={this.state.date}
+      />
+      </p1>
+      <Divider
+          as='h4'
+          className='header'
+          horizontal
+          style={{ margin: '1em 0em', textTransform: 'uppercase' }}
+        >
+        </Divider>
+        <p1>
+        todolist
+        </p1>
+      </Container>
 
 
       </div>
