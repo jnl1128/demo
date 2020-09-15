dcbb82f123a9482c4829929022d26a5b00ca83f5
Narin <jnl1128@naver.com>
2020-09-15 21:50:10 +0900

    signup_pages

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+/*import React, {Component} from 'react';
+
 class Signup extends Component {
     render() {
         return (
@@ -8,4 +9,127 @@ class Signup extends Component {
         )
     }
 };
-export default Signup;
+export default Signup;*/
+
+import React from 'react'
+import Register from 'components/Register';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Button,
+  Icon,
+} from 'semantic-ui-react'
+import logo from 'image/WEBOARD.jpg';
+
+const FixedMenuLayout = () => (
+  <div>
+  <Menu fixed='top'inverted >
+      <Container>
+        <Menu.Item as='a' header >
+          <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
+          WE:BOARD
+        </Menu.Item>
+        <Menu.Item as='a'>
+          <Link to="/Classroom">
+          <Icon name = "student"/> 교실</Link>
+        </Menu.Item>
+        <Menu.Item as='a'>
+          <Link to="/Monthly">
+          <Icon name = "calendar"/> 달력</Link>
+        </Menu.Item>
+​
+        {/*<Dropdown item simple text='Dropdown'>
+          <Dropdown.Menu>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Header Item</Dropdown.Header>
+            <Dropdown.Item>
+              <i className='dropdown icon' />
+              <span className='text'>Submenu</span>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>*/}
+
+      </Container>
+    </Menu>
+​
+    <Container text style={{ marginTop: '7em' }}>
+      <Register/>
+    </Container>
+
+<Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+      <Container textAlign='center'>
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Group 1' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+                <List.Item as='a'>Link Two</List.Item>
+                <List.Item as='a'>Link Three</List.Item>
+                <List.Item as='a'>Link Four</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Group 2' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+                <List.Item as='a'>Link Two</List.Item>
+                <List.Item as='a'>Link Three</List.Item>
+                <List.Item as='a'>Link Four</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Group 3'/>
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+                <List.Item as='a'>Link Two</List.Item>
+                <List.Item as='a'>Link Three</List.Item>
+                <List.Item as='a'>Link Four</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header inverted as='h4' content='Footer Header' />
+              <p>
+                Extra space for a call to action inside the footer that could help re-engage users.
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+​
+        <Divider inverted section />
+        <Image centered size='mini' src='/logo.png' />
+        <List horizontal inverted divided link>
+          <List.Item as='a' href='#'>
+            Site Map
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Contact Us
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Terms and Conditions
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Privacy Policy
+          </List.Item>
+        </List>
+      </Container>
+    </Segment>
+
+  </div>
+)
+
+export default FixedMenuLayout
